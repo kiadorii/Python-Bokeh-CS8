573c68216ee1bf5512451367261ddc4888624988
kiadorii <sgchoi529@email.arizona.edu>
2018-06-20 16:34:51 -0700

    Able to retrieve values!

--- src/draw.py
+++ src/draw.py
@@ -2,16 +2,15 @@ import math
 
 from bokeh.io import show, output_file
 from bokeh.plotting import figure
-from bokeh.models import GraphRenderer, StaticLayoutProvider, Oval
+from bokeh.models import GraphRenderer, StaticLayoutProvider, Oval, LabelSet, Label
 from bokeh.palettes import Spectral8
 
 from graph import *
 
 graph_data = Graph()
 graph_data.debug_create_test_data()
-# print(graph_data.vertexes)
 
-N = 10
+N = len(graph_data.vertexes)
 node_indices = list(range(N))
 
 # debug_pallete = Spectral8
@@ -28,7 +27,7 @@ graph = GraphRenderer()
 
 graph.node_renderer.data_source.add(node_indices, 'index')
 graph.node_renderer.data_source.add(color_list, 'color')
-graph.node_renderer.glyph = Oval(height=10, width=10, fill_color='color')
+graph.node_renderer.glyph = Oval(height=20, width=25, fill_color='color')
 
 # this is drawing the edges from start to end
 
@@ -38,12 +37,18 @@ graph.edge_renderer.data_source.data = dict(
 
 ### start of layout code
 # Looks like this is setting the positions of the vertexes
-# circ = [i*2*math.pi/8 for i in node_indices] - we don't want a circle
+# circ = [i*2*math.pi/8 for i in node_indices] # - we don't want a circle
 # x = [math.cos(i) for i in circ] # This is assembling a list for us
 # y = [math.sin(i) for i in circ]
+val = [v.value[::] for v in graph_data.vertexes]
 x = [v.pos['x'] for v in graph_data.vertexes]
 y = [v.pos['y'] for v in graph_data.vertexes]
+# vertex_name = [v.name['a'] for names in graph_data.vertexes]
+print(val)
 
+# source = ColumnDataSource(data=dict(x, y, graph_data.debug_create_test_data))
+# labels = LabelSet(x='weight', y='height', text='names', level='glyph',
+#               x_offset=5, y_offset=5, source=source, render_mode='canvas')
 graph_layout = dict(zip(node_indices, zip(x, y)))
 graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
 
