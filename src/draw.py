29c5148a8137e85c7c5bacc897dfbb6f14e3c77e
kiadorii <sgchoi529@email.arizona.edu>
2018-06-20 10:49:53 -0700

    Working debug_create_test_data()

--- src/draw.py
+++ src/draw.py
@@ -5,17 +5,27 @@ from bokeh.plotting import figure
 from bokeh.models import GraphRenderer, StaticLayoutProvider, Oval
 from bokeh.palettes import Spectral8
 
-N = 8
+from graph import *
+
+graph_data = Graph()
+graph_data.debug_create_test_data()
+print(graph_data.vertexes)
+
+N = 10
 node_indices = list(range(N))
 
-plot = figure(title='Graph Layout Demonstration', x_range=(-1.1,1.1), y_range=(-1.1,1.1),
+debug_pallete = Spectral8
+debug_pallete.append('#ff0000')
+debug_pallete.append('#0000ff')
+
+plot = figure(title='Graph Layout Demonstration', x_range=(0, 500), y_range=(0, 500),
               tools='', toolbar_location=None)
 
 graph = GraphRenderer()
 
 graph.node_renderer.data_source.add(node_indices, 'index')
 graph.node_renderer.data_source.add(Spectral8, 'color')
-graph.node_renderer.glyph = Oval(height=0.1, width=0.2, fill_color='color')
+graph.node_renderer.glyph = Oval(height=10, width=10, fill_color='color')
 
 graph.edge_renderer.data_source.data = dict(
     start=[0]*N,
