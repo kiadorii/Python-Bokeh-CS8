c18c2487b004f8dbb9cdc17d3c0e983b96cedc81
kiadorii <sgchoi529@email.arizona.edu>
2018-06-21 13:55:53 -0700

    Initial labels showing - Bokeh

--- src/draw.py
+++ src/draw.py
@@ -68,5 +68,23 @@ graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
 
 plot.renderers.append(graph)
 
+
+source = ColumnDataSource(data=dict(height=[66, 71, 72, 68, 58, 62],
+                                    weight=[165, 189, 220, 141, 260, 174],
+                                    names=['Mark', 'Amir', 'Matt', 'Greg',
+                                           'Owen', 'Juan']))
+
+
+labels = LabelSet(x='weight', y='height', text='names', level='glyph',
+              x_offset=5, y_offset=5, source=source, render_mode='canvas')
+
+citation = Label(x=70, y=70, x_units='screen', y_units='screen',
+                 text='Collected by Luke C. 2016-04-01', render_mode='css',
+                 border_line_color='black', border_line_alpha=1.0,
+                 background_fill_color='white', background_fill_alpha=1.0)
+
+plot.add_layout(labels)
+plot.add_layout(citation)
+
 output_file('graph.html')
 show(plot)
