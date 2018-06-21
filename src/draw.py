d31ac5803df7c5d341861d5f12c37a9a68b2ac6f
kiadorii <sgchoi529@email.arizona.edu>
2018-06-21 14:56:40 -0700

    No Errors but Image not Rendering - Oval to Circle

--- src/draw.py
+++ src/draw.py
@@ -2,7 +2,7 @@ import math
 
 from bokeh.io import show, output_file
 from bokeh.plotting import figure
-from bokeh.models import GraphRenderer, StaticLayoutProvider, Oval, ColumnDataSource, RangeId, LabelSet, Label
+from bokeh.models import GraphRenderer, StaticLayoutProvider, Circle, ColumnDataSource, Range1d, LabelSet, Label
 from bokeh.palettes import Spectral8
 
 from graph import *
@@ -27,16 +27,16 @@ graph = GraphRenderer()
 
 graph.node_renderer.data_source.add(node_indices, 'index')
 graph.node_renderer.data_source.add(color_list, 'color')
-graph.node_renderer.glyph = Oval(height=20, width=25, fill_color='color')
+graph.node_renderer.glyph = Circle(height=20, width=25, fill_color='color')
 
 # this is drawing the edges from start to end
 # This retrieves the vertexes dynamically (rather than static)
 start_indexes = []
 end_indexes = []
 
-for start_indexes, vertex in enumerate(graph_data.vertexes): # use enumerate to get the indexes
+for start_index, vertex in enumerate(graph_data.vertexes): # use enumerate to get the indexes
     for e in vertex.edges:
-        start_indexes.append(start_indexes)
+        start_indexes.append(start_index)
         end_indexes.append(graph_data.vertexes.index(e.destination))
 
 graph.edge_renderer.data_source.data = dict(
@@ -54,11 +54,9 @@ graph.edge_renderer.data_source.data = dict(
 # circ = [i*2*math.pi/8 for i in node_indices] # - we don't want a circle
 # x = [math.cos(i) for i in circ] # This is assembling a list for us
 # y = [math.sin(i) for i in circ]
-val = [v.value[::] for v in graph_data.vertexes]
+# val = [v.value[::] for v in graph_data.vertexes]
 x = [v.pos['x'] for v in graph_data.vertexes]
 y = [v.pos['y'] for v in graph_data.vertexes]
-# vertex_name = [v.name['a'] for names in graph_data.vertexes]
-print(val)
 
 # source = ColumnDataSource(data=dict(x, y, graph_data.debug_create_test_data))
 # labels = LabelSet(x='weight', y='height', text='names', level='glyph',
@@ -74,17 +72,18 @@ source = ColumnDataSource(data=dict(height=[66, 71, 72, 68, 58, 62],
                                     names=['Mark', 'Amir', 'Matt', 'Greg',
                                            'Owen', 'Juan']))
 
+# Create a new dictionary to use as a data source, with three lists in it, ordered in the same way as vertexes
+# List of x values 
+# List of y values
+# List of labels
+value = [v.value for v in graph_data.vertexes] # Possible optimization: We run through this loop three times
+label_source = ColumnDataSource(data=dict(x = x, y = y, v = value))
 
-labels = LabelSet(x='weight', y='height', text='names', level='glyph',
+labels = LabelSet(x='x', y='y', text='v', level='glyph',
               x_offset=5, y_offset=5, source=source, render_mode='canvas')
 
-citation = Label(x=70, y=70, x_units='screen', y_units='screen',
-                 text='Collected by Luke C. 2016-04-01', render_mode='css',
-                 border_line_color='black', border_line_alpha=1.0,
-                 background_fill_color='white', background_fill_alpha=1.0)
-
+# TODO: Investigate plot.add_layout vs. plot.renderers.append
 plot.add_layout(labels)
-plot.add_layout(citation)
 
 output_file('graph.html')
 show(plot)
